a9cf5f5e906d21e0956588a4275fd361c18f31f6
MBANS8A1 <seanmbng@aol.com>
2025-06-18 14:17:05 +0100

    Within the row, I added a Column with two text nodes and a spacer. Also, the pet image has its aspect ratio maintained as is equal to the dimensions of the destination with contentScale being ContentScale.Crop. Features of the mock-up pet added.

--- app/src/main/java/com/example/petrescueapp/components/PetInfo.kt
+++ app/src/main/java/com/example/petrescueapp/components/PetInfo.kt
@@ -1,13 +1,23 @@
 package com.example.petrescueapp.components
 
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.petrescueapp.R
@@ -19,14 +29,35 @@ fun PetInfoItem() {
         .padding(16.dp)
     ){
         Row{
-            Image(modifier = Modifier.size(80.dp,80.dp),
+            Image(modifier = Modifier.size(80.dp,80.dp)
+                .clip((RoundedCornerShape(16.dp)))
+                ,
                 painter= painterResource(R.drawable.blue_dog),
-                contentDescription="")
+                contentDescription="",
+                contentScale = ContentScale.Crop)
+            Spacer(modifier=Modifier.width(16.dp))
+
+            Column {
+                Text("Pogo",
+                    color = MaterialTheme.colorScheme.onSurface,
+                    fontWeight = FontWeight.Bold,
+                    style = MaterialTheme.typography.headlineSmall
+                )
+                Spacer(modifier=Modifier.height(8.dp))
+                Text(
+                    text = buildString {
+                        append("Adult")
+                        append("|")
+                        append("Domestic Short Hair")
+                    }
+                )
+
+            }
         }
     }
 }
 
-@Preview
+@Preview(showBackground = true)
 @Composable
 private fun PrevPetInfo() {
     PetInfoItem()
