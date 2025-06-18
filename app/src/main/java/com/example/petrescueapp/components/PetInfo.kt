7dc40b7f60861f71355249cbd4d9d71596a0bc5c
MBANS8A1 <seanmbng@aol.com>
2025-06-18 07:31:43 +0100

    Decided to create a UI-specific branch to work on the PetInfo implementation.

--- app/src/main/java/com/example/petrescueapp/components/PetInfo.kt
+++ app/src/main/java/com/example/petrescueapp/components/PetInfo.kt
@@ -1,9 +1,19 @@
 package com.example.petrescueapp.components
 
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun(modifier: Modifier = Modifier) {
+fun PetInfoItem() {
+    Row(modifier = Modifier.fillMaxWidth()
+        .padding(16.dp)
+    ){
+        Row{
 
+        }
+    }
 }
