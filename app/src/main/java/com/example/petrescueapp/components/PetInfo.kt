e04bc8f7ac015d4ca5b76da74920cf8b3c7bf12e
MBANS8A1 <seanmbng@aol.com>
2025-06-18 06:45:41 +0100

    Added a new PetInfo.kt file.

---
+++ app/src/main/java/com/example/petrescueapp/components/PetInfo.kt
@@ -0,0 +1,9 @@
+package com.example.petrescueapp.components
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun(modifier: Modifier = Modifier) {
+
+}
