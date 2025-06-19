e2fcba3b14b41b93648a52c159906010eaae06b2
MBANS8A1 <seanmbng@aol.com>
2025-06-19 10:05:28 +0100

    Create a reusable composable function for assigning gender text within a Box layout. This we be used on the right-side column.

--- app/src/main/java/com/example/petrescueapp/components/PetInfo.kt
+++ app/src/main/java/com/example/petrescueapp/components/PetInfo.kt
@@ -1,7 +1,9 @@
 package com.example.petrescueapp.components
 
-import android.graphics.Color
+
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -10,6 +12,7 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
@@ -17,11 +20,14 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.room.util.copy
 import com.example.petrescueapp.R
 
 @Composable
@@ -75,6 +81,34 @@ fun PetInfoItem() {
                 }
             }
         }
+        Column {
+
+        }
+    }
+}
+
+@Composable
+fun GenderTag(gender:String,modifier:Modifier){
+    val colour = if(gender == "Male"){
+        Color.Blue
+    }else{
+        Color.Red
+    }
+    Box(
+        modifier = Modifier
+            .wrapContentSize()
+            .clip(RoundedCornerShape(12.dp))
+            .background(colour.copy(alpha=0.2f))
+
+
+    ){
+        Text(
+            text=gender,
+            modifier = Modifier.padding(12.dp,4.dp,12.dp,6.dp),
+            style = MaterialTheme.typography.bodySmall,
+            color= colour
+
+        )
     }
 }
 
