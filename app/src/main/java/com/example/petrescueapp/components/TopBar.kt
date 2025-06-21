b8675d6f42429d85bbe5ca56121336c9c56dbbba
MBANS8A1 <seanmbng@aol.com>
2025-06-21 14:11:52 +0100

    Within the components folder added the TopBar composable with a Row and nested Column composable in a new Kotlin file.

--- app/src/main/java/com/example/petrescueapp/components/TopBar.kt
+++ app/src/main/java/com/example/petrescueapp/components/TopBar.kt
@@ -1,9 +1,24 @@
 package com.example.petrescueapp.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 
 @Composable
 fun TopBar() {
-    
+    Row(modifier = Modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ){
+        Column(modifier = Modifier.padding(16.dp)) {
+            Text(text="Hi Sean,",textAlign=TextAlign.Start)
+        }
+
+    }
 }
