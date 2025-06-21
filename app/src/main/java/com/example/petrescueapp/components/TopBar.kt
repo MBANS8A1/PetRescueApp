63b01afdbdc90d05feb1c5218cc4fa1d8f904653
MBANS8A1 <seanmbng@aol.com>
2025-06-21 19:38:03 +0100

    Added a TopBar of with a Column composable with texts that have been styled and coloured.Now finishing logic for the PetSwitch composable.

--- app/src/main/java/com/example/petrescueapp/components/TopBar.kt
+++ app/src/main/java/com/example/petrescueapp/components/TopBar.kt
@@ -1,15 +1,25 @@
 package com.example.petrescueapp.components
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.petrescueapp.R
 
 @Composable
 fun TopBar() {
@@ -17,8 +27,40 @@ fun TopBar() {
         horizontalArrangement = Arrangement.SpaceBetween
     ){
         Column(modifier = Modifier.padding(16.dp)) {
-            Text(text="Hi Sean,",textAlign=TextAlign.Start)
+            Text(text="Hi Sean,",textAlign=TextAlign.Start,
+                style= MaterialTheme.typography.headlineMedium,
+                color=MaterialTheme.colorScheme.onSurface
+                )
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text="Find a new pet near you to adopt!",textAlign=TextAlign.Start,
+                style= MaterialTheme.typography.bodySmall,
+                color=MaterialTheme.colorScheme.onSurface
+            )
         }
 
     }
+}
+
+@Composable
+fun PetSwitch(onSwitchToggle:() -> Unit) {
+    val icon = if(isSystemInDarkTheme()){
+        painterResource(R.drawable.ic_switch_on)
+    }
+    else{
+        painterResource(R.drawable.ic_switch_off)
+    }
+
+    Icon(painter= icon, contentDescription = null,
+        modifier=Modifier.size(24.dp,24.dp)
+        .clickable(
+            onClick = {onSwitchToggle.invoke()}
+        ),
+        tint = MaterialTheme.colorScheme.onSurface
+}
+
+@Preview
+@Composable
+private fun PrevTopBar() {
+    TopBar()
+    
 }
