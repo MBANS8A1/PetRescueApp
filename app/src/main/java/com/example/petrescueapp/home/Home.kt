dfd9f6fe2081038273afe10c1dcf5a1379e35341
MBANS8A1 <seanmbng@aol.com>
2025-06-21 10:25:32 +0100

    Added a Scaffold and top bar with padding values to the Home composable.

--- app/src/main/java/com/example/petrescueapp/home/Home.kt
+++ app/src/main/java/com/example/petrescueapp/home/Home.kt
@@ -2,11 +2,13 @@ package com.example.petrescueapp.home
 
 import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -18,14 +20,21 @@ import com.example.petrescueapp.data.DummyPetDataSource
 @Composable
 fun Home() {
     val petList = DummyPetDataSource.dogList
-    LazyColumn {
-        items(petList){pet->
-            PetInfoItem(pet) {
+    Scaffold(
+        topBar = {
 
+        }
+    ) {
+        paddingValues: PaddingValues ->
+        LazyColumn(contentPadding = paddingValues) {
+            items(petList){pet->
+                PetInfoItem(pet) {
+
+                }
             }
         }
-    }
 
+    }
 }
 
 @Preview
