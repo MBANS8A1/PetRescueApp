36cdc986716ba44f04cb08e07e113b2a1e4a5f2b
MBANS8A1 <seanmbng@aol.com>
2025-06-22 17:13:43 +0100

    I decided to separate the logic of the pet information as a different component in a PetBasicInfo.kt file. I am using a composable with name, gender and location as parameters. I am working on the nested layout currently.

---
+++ app/src/main/java/com/example/petrescueapp/components/PetBasicInfo.kt
@@ -0,0 +1,24 @@
+package com.example.petrescueapp.components
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun PetBasicInfo(
+    name:String,
+    gender:String,
+    location:String
+) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+    ){
+
+    }
+
+}
