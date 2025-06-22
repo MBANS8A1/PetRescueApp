209da88dbf0a85aaea5ccf2c678d2e79d341cf8b
MBANS8A1 <seanmbng@aol.com>
2025-06-22 11:21:11 +0100

    Added the attributes for the topBar using navigationIcon, Icon and colors as necessary to show the text and back button as needed. I will now work on the image and other column-based content.

--- app/src/main/java/com/example/petrescueapp/detail/Detail.kt
+++ app/src/main/java/com/example/petrescueapp/detail/Detail.kt
@@ -1,18 +1,29 @@
 package com.example.petrescueapp.detail
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarColors
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.contentColorFor
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DetailScreen(onNavigate:()->Unit) {
     Scaffold(
@@ -22,18 +33,35 @@ fun DetailScreen(onNavigate:()->Unit) {
                     Text(text="Detail")
                 },
                 navigationIcon = {
+                    Spacer(modifier = Modifier.width(50.dp))
                     Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
                         contentDescription = null,
-                        modifier = Modifier.size(24.dp,24.dp)
-                        .clickable{
-                            onNavigate.invoke()
-                        }
+                        modifier = Modifier
+                            .size(24.dp, 24.dp)
+                            .clickable {
+                                onNavigate.invoke()
+                            },
+                        tint = MaterialTheme.colorScheme.onSurface
                     )
-
-                }
+                },
+                colors = TopAppBarDefaults
+                    .topAppBarColors(containerColor = MaterialTheme.colorScheme.background,
+                        titleContentColor = MaterialTheme.colorScheme.onSurface,
+                        actionIconContentColor = MaterialTheme.colorScheme.onSurface
+                    )
             )
         }
-    ) {
+    ) {padding ->
+        LazyColumn(contentPadding = padding) {
 
+        }
     }
 }
+
+@Preview(showSystemUi = true)
+@Composable
+private fun PrevDetailScreen() {
+    DetailScreen {  }
+}
+
+
