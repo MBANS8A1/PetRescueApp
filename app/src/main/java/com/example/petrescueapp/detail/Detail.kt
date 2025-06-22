187edb98444fbd9301c6702d50e3320dbfb8fe83
MBANS8A1 <seanmbng@aol.com>
2025-06-22 09:17:25 +0100

    Passed the clickable event back up the caller for the onNagivate that is passed as a parameter.

--- app/src/main/java/com/example/petrescueapp/detail/Detail.kt
+++ app/src/main/java/com/example/petrescueapp/detail/Detail.kt
@@ -1,16 +1,20 @@
 package com.example.petrescueapp.detail
 
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 
 @Composable
-fun DetailScreen() {
+fun DetailScreen(onNavigate:()->Unit) {
     Scaffold(
         topBar = {
             TopAppBar(
@@ -18,7 +22,14 @@ fun DetailScreen() {
                     Text(text="Detail")
                 },
                 navigationIcon = {
-                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack, contentDescription = null)
+                    Icon(imageVector = Icons.AutoMirrored.Filled.ArrowBack,
+                        contentDescription = null,
+                        modifier = Modifier.size(24.dp,24.dp)
+                        .clickable{
+                            onNavigate.invoke()
+                        }
+                    )
+
                 }
             )
         }
