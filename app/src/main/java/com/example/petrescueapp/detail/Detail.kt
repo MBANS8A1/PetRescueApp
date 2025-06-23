e585b671bf614ec594109fd97869816e6ba7e3d6
MBANS8A1 <seanmbng@aol.com>
2025-06-23 14:01:50 +0100

    Now working on adding the pet story to a seperate item lambda below the pet basic info and image item. I took the story item Column out as a separate composable and as well as the title. I may reuse the title in the future so I did not want to hardcode specific titles.

--- app/src/main/java/com/example/petrescueapp/detail/Detail.kt
+++ app/src/main/java/com/example/petrescueapp/detail/Detail.kt
@@ -2,10 +2,12 @@ package com.example.petrescueapp.detail
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -26,10 +28,13 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.petrescueapp.components.PetBasicInfo
 import com.example.petrescueapp.data.DummyPetDataSource
+import com.example.petrescueapp.data.model.Pet
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
@@ -66,15 +71,43 @@ fun DetailScreen(index:Int,onNavigate:()->Unit) {
             item {
                 Image(painter = painterResource(id=pet.image),
                     contentDescription = null,
-                    modifier = Modifier.fillMaxWidth()
+                    modifier = Modifier
+                        .fillMaxWidth()
                         .height(346.dp),
                     alignment = Alignment.CenterStart,
                     contentScale = ContentScale.Crop)
                 Spacer(modifier = Modifier.height(16.dp))
                 PetBasicInfo(name=pet.name,gender=pet.gender, location = pet.location)
             }
+            item{
+
+            }
         }
     }
+}
+
+@Composable
+fun MyStoryItem(pet: Pet) {
+    Column {
+        Spacer(modifier = Modifier.height(24.dp))
+        Title(title="My Story")
+    }
+}
+
+@Composable
+fun Title(title:String) {
+    Text(
+        text=title,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(start = 16.dp)
+
+        ,
+        color = MaterialTheme.colorScheme.onSurface,
+        style = MaterialTheme.typography.titleSmall,
+        fontWeight = FontWeight.W600,
+        textAlign = TextAlign.Start
+    )
 }
 
 @Preview(showSystemUi = true)
