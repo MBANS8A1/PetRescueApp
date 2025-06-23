19ab2e112f723abf6d5ee4d78674caad7e4f6a6c
MBANS8A1 <seanmbng@aol.com>
2025-06-23 09:06:16 +0100

    Added a nested layout with a Column for the pet name that contains a spacer and a row with a navigation symbol and a location text. Styling to the font and well as colour has been applied as needed along with alignment adjustments.

--- app/src/main/java/com/example/petrescueapp/components/PetBasicInfo.kt
+++ app/src/main/java/com/example/petrescueapp/components/PetBasicInfo.kt
@@ -1,11 +1,26 @@
 package com.example.petrescueapp.components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.input.pointer.motionEventSpy
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.petrescueapp.R
 
 @Composable
 fun PetBasicInfo(
@@ -16,9 +31,44 @@ fun PetBasicInfo(
     Row(
         modifier = Modifier
             .fillMaxWidth()
-            .padding(16.dp)
+            .padding(16.dp),
+        horizontalArrangement = Arrangement.SpaceBetween
     ){
+        Column(modifier = Modifier.align(Alignment.CenterVertically)) {
+            Text(text=name,
+                modifier = Modifier.padding(start = 0.5.dp, top=12.dp, end=12.dp, bottom=12.dp),
+                color = MaterialTheme.colorScheme.onSurface,
+                fontWeight = FontWeight.Bold,
+                style= MaterialTheme.typography.headlineMedium
+            )
+            Spacer(modifier = Modifier.height(8.dp))
+            Row(verticalAlignment = Alignment.Bottom) {
+                Icon(painterResource(R.drawable.ic_location), contentDescription =null,
+                    modifier = Modifier.size(16.dp,16.dp),
+                    tint= Color.Red
+                )
+                Text(text=location, modifier = Modifier
+                    .padding(
+                        start = 8.dp,
+                        top= 0.dp,
+                        end = 12.dp,
+                        bottom= 0.dp
+
+                    ),
+                    color = MaterialTheme.colorScheme.onSurface,
+                    style= MaterialTheme.typography.bodySmall
+                )
+            }
+
+        }
 
     }
+
+}
+
+@Preview
+@Composable
+private fun PrevBasicInfoItem() {
+    PetBasicInfo(name="Roller",gender="Male", location = "Toronto CA")
 
 }
