c155e79841156db0446bc5c57b684ca8619e2f4c
MBANS8A1 <seanmbng@aol.com>
2025-06-24 14:18:45 +0100

    I have added the owner name and basic information of the owner (occupation) within Texts in a Column. I am now working on adding the right-aligned icon.

--- app/src/main/java/com/example/petrescueapp/components/OwnerCardInfo.kt
+++ app/src/main/java/com/example/petrescueapp/components/OwnerCardInfo.kt
@@ -2,19 +2,25 @@ package com.example.petrescueapp.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.petrescueapp.data.DummyPetDataSource
@@ -38,6 +44,21 @@ fun OwnerCardInfo(owner: Owner) {
                 contentScale = ContentScale.Crop
 
                 )
+            Spacer(modifier = Modifier.width(16.dp))
+            Column {
+                Text(text= owner.name,
+                    color =  MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.labelMedium,
+                    fontWeight = FontWeight.W600,
+                    textAlign = TextAlign.Start
+                )
+                Spacer(modifier = Modifier.height(8.dp))
+                Text(text= owner.basicInfo,
+                    color =  MaterialTheme.colorScheme.onSurface,
+                    style = MaterialTheme.typography.labelSmall
+                )
+
+            }
         }
 
     }
