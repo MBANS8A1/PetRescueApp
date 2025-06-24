f93d20cfbd74621a01ada09878958840469ef8e1
MBANS8A1 <seanmbng@aol.com>
2025-06-24 13:21:49 +0100

    I have created part of the Owner card layout with a nested Row for the image and text. It still needs to be completed. I have added a preview composable function to check the alignment of the pet image.

--- app/src/main/java/com/example/petrescueapp/components/OwnerCardInfo.kt
+++ app/src/main/java/com/example/petrescueapp/components/OwnerCardInfo.kt
@@ -1,2 +1,51 @@
 package com.example.petrescueapp.components
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.petrescueapp.data.DummyPetDataSource
+import com.example.petrescueapp.data.model.Owner
+
+@Composable
+fun OwnerCardInfo(owner: Owner) {
+    Spacer(modifier = Modifier.height(16.dp))
+    Row(horizontalArrangement = Arrangement.SpaceBetween,
+        verticalAlignment = Alignment.CenterVertically,
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)) {
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Image(painter= painterResource(owner.image),
+                contentDescription =null,
+                modifier = Modifier
+                    .size(60.dp, 60.dp)
+                    .clip(RoundedCornerShape(16.dp)),
+                alignment = Alignment.CenterStart,
+                contentScale = ContentScale.Crop
+
+                )
+        }
+
+    }
+}
+
+@Preview
+@Composable
+private fun PrevOwnerCard() {
+    OwnerCardInfo(owner = DummyPetDataSource.dogList[0].owner)
+    
+}
