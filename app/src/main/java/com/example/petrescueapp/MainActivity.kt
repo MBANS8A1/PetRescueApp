8b81c72430bbaedf608620379afe053a62469b74
MBANS8A1 <seanmbng@aol.com>
2025-06-25 11:17:20 +0100

    Now in the setContent I have decided to use state hoisting to control what screen I am on. I am usinf the selectedIndex of the pet to keep track of a pet when a particular pet is clicked and pass it to the onPetClick on the Home composable. The screens are being held in a Screen enum.

--- app/src/main/java/com/example/petrescueapp/MainActivity.kt
+++ app/src/main/java/com/example/petrescueapp/MainActivity.kt
@@ -11,25 +11,50 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.petrescueapp.detail.DetailScreen
 import com.example.petrescueapp.home.Home
 import com.example.petrescueapp.ui.theme.PetRescueAppTheme
 
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            var currentScreen by remember{
+                mutableStateOf(Screen.Home)
+            }
+            var selectedIndex by remember{
+                mutableIntStateOf(-1)
+            }
             PetRescueAppTheme {
                 Surface(modifier = Modifier.fillMaxSize(),
                     color= MaterialTheme.colorScheme.background
                     ){
-                    DetailScreen(index = 0) { }
+                    when(currentScreen){
+                        Screen.Home ->{
+                            Home(onSwitchClick = ,
+                                onPetClick ={ index->
+                                    currentScreen = Screen.Detail
+                                    selectedIndex = index
+                                } )
+                        }
+
+                    }
                 }
             }
         }
     }
 }
 
+enum class Screen{
+    Home,
+    Detail
+}
