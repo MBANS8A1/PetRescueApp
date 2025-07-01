5568a0eaf0935030d6e1e7a016875405c3be13ec
MBANS8A1 <seanmbng@aol.com>
2025-07-01 15:16:55 +0100

    In the build.gradle (Module app) under dependencies, I have added some extra dependencies to support in making requests to the Petfinder API. Such as dependencies for retrofit, serialization, logging and the viewmodel.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,4 +57,16 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+
+
+    //network
+    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.okhttp3:okhttp:4.9.0")
+    implementation("com.squareup.okhttp3:logging-interceptor:4.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    // Lifecycle components
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.1")
+    implementation ("androidx.lifecycle:lifecycle-common-java8:2.6.1")
 }
