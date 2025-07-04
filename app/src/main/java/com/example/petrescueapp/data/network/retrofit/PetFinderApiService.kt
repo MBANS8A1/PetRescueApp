627cd3828a193541e821152ba5c58c91ab24534d
MBANS8A1 <seanmbng@aol.com>
2025-07-04 11:27:46 +0100

    I have created a new interface file within the network subdirectory called retrofit. PetFinderApiService.kt has GET and POST asynchronous suspend functions to return the list of animals and get the access token (using the API and Secret keys), respectively. Pagination for the GET request has been taken into account with the @Query annotation. I still have to generate an Access Token model from the JSON on the Purina Petfinder API docs site.

---
+++ app/src/main/java/com/example/petrescueapp/data/network/retrofit/PetFinderApiService.kt
@@ -0,0 +1,31 @@
+package com.example.petrescueapp.data.network.retrofit
+
+import com.example.petrescueapp.data.network.models.ApiAnimals
+import com.example.petrescueapp.data.network.models.baseObject.API_KEY
+import com.example.petrescueapp.data.network.models.baseObject.AUTH_ENDPOINT
+import com.example.petrescueapp.data.network.models.baseObject.BASE_ENDPOINT
+import com.example.petrescueapp.data.network.models.baseObject.CLIENT_ID
+import com.example.petrescueapp.data.network.models.baseObject.CLIENT_SECRET
+import com.example.petrescueapp.data.network.models.baseObject.SECRET_KEY
+import retrofit2.http.Field
+import retrofit2.http.FormUrlEncoded
+import retrofit2.http.GET
+import retrofit2.http.POST
+import retrofit2.http.Query
+
+//suspend function(s) for making asynchronous API requests
+interface PetFinderApiService {
+    @GET(BASE_ENDPOINT)
+    suspend fun getAnimals(
+       @Query("page") page : Int
+    ) :ApiAnimals
+
+
+    @POST(AUTH_ENDPOINT)
+    @FormUrlEncoded
+    suspend fun getAuthToken(
+        @Field(CLIENT_ID) clientId: String = API_KEY,
+        @Field(CLIENT_SECRET) clientSecret: String = SECRET_KEY,
+        @Field("grant_type") grantType: String = "client_Credentials"
+    )
+}
