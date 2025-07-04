09d1084c7c540bb52496a86e4b39ca8b6b1f6691
MBANS8A1 <seanmbng@aol.com>
2025-07-04 20:03:13 +0100

    In the main PetRescue app package, I created a package called token and inside made an interface file called AccessTokenProvider.kt. This will act as our session manager and will contain method signatures to help us manage the access token (including fetching the access token, getting the token string, saving the token to local storage and applying a lock for synchronization reasons). I wil create a file to implement these methods.

---
+++ app/src/main/java/com/example/petrescueapp/token/AccessTokenProvider.kt
@@ -0,0 +1,16 @@
+package com.example.petrescueapp.token
+
+import com.example.petrescueapp.data.network.models.AccessToken
+
+interface AccessTokenProvider {
+    suspend fun fetchAccessToken():AccessToken?
+
+    fun refreshToken():AccessToken?
+
+    fun saveToken(token:String?)
+
+    fun token():String?
+
+    fun lock(): Any
+
+}
