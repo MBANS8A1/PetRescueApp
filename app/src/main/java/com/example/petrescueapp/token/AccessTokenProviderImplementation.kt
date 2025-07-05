4142cd6d2ddcb6f1efc01ec6b685e3abaa793f20
MBANS8A1 <seanmbng@aol.com>
2025-07-05 15:01:31 +0100

    In the AccessTokenProviderImplementation.kt file, I have added the code to implement the fetchAccessToken() method. In this case, I have used Kotlin's Dispatchers.IO to switch to a new lightweight thread when this method is called. When the access token is fetched with the Graph object apiService getAuthToken() method, the access token's string is saved in local storage; afterwards, the function returns the access token.

--- app/src/main/java/com/example/petrescueapp/token/AccessTokenProviderImplementation.kt
+++ app/src/main/java/com/example/petrescueapp/token/AccessTokenProviderImplementation.kt
@@ -1,13 +1,19 @@
 package com.example.petrescueapp.token
 
+import com.example.petrescueapp.Graph
 import com.example.petrescueapp.data.local.StoragePref
 import com.example.petrescueapp.data.network.models.AccessToken
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
 
 class AccessTokenProviderImplementation(
     private val storagePref : StoragePref
 ):AccessTokenProvider {
-    override suspend fun fetchAccessToken(): AccessToken? {
-        TODO("Not yet implemented")
+    override suspend fun fetchAccessToken(): AccessToken? = withContext(
+        Dispatchers.IO){
+        val accessToken = Graph.apiService.getAuthToken()
+        saveToken(accessToken.accessToken)
+        accessToken //returning the access token
     }
 
     override fun refreshToken(): AccessToken? {
