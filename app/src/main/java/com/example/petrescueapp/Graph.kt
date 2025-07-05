e3570b206a7d25423ce484961500c538f7945a39
MBANS8A1 <seanmbng@aol.com>
2025-07-05 12:54:02 +0100

    I created a Graph.kt object file in the main app package. I made a provide function that initializes the lateinit variables of tokenStoragePref and apiService. These are important for getting/saving the token and accessing the data we need from the Petfinder API. This object file help to build the retrofit instance. The retrofit instance is informed via the converter factory that it should expect a JSON response as its media type. Also, I am ignoring any unknown properties in the server response that could cause a SerializationException, when the retrofit instance contacts the server with base URL.

---
+++ app/src/main/java/com/example/petrescueapp/Graph.kt
@@ -0,0 +1,37 @@
+package com.example.petrescueapp
+
+import android.content.Context
+import com.example.petrescueapp.data.local.StoragePref
+import com.example.petrescueapp.data.network.models.baseObject
+import com.example.petrescueapp.data.network.retrofit.PetFinderApiService
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+private val json = Json { ignoreUnknownKeys - true }
+
+
+object Graph {
+    //For getting the token and saving the token
+    lateinit var tokenStoragePref : StoragePref
+    //For getting an authorised token and list of animals
+    lateinit var apiService :  PetFinderApiService
+
+    fun provide(context:Context){
+        tokenStoragePref = StoragePref(context)
+        apiService = createPetFinderApiService()
+    }
+
+    private fun createPetFinderApiService():PetFinderApiService{
+        //telling retrofit data will come to me in the form of JSON
+        val contentType = "application/json".toMediaType()
+        return Retrofit.Builder()
+            .baseUrl(baseObject.BASE_URL)
+            .addConverterFactory(json
+                    .asConverterFactory(contentType)
+            )
+            .build()
+            .create(PetFinderApiService::class.java)
+    }
+}
