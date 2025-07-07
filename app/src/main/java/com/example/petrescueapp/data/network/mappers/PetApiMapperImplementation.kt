b666d6f038dc5861ebb1f00b54a81bcd12a08940
MBANS8A1 <seanmbng@aol.com>
2025-07-07 21:53:50 +0100

    In the overrode mapToDomain() method, I have used the map method on the animals data returned from the network to generate a Pet that can be put into a List. However, there are some formatting methods I have added to account for the structure for the data. The formatData() will return the entity data or an empty string if no data is registered with that entity. The formatAddress() method will put a period in between the segments of the address and then that is combined with formatData() to fill in the contacts information for a pet owner in formatContacts(), which I still need to complete along with the mapToDomain().

--- app/src/main/java/com/example/petrescueapp/data/network/mappers/PetApiMapperImplementation.kt
+++ app/src/main/java/com/example/petrescueapp/data/network/mappers/PetApiMapperImplementation.kt
@@ -1,4 +1,44 @@
 package com.example.petrescueapp.data.network.mappers
 
-class PetApiMapperImplementation {
+import com.example.petrescueapp.data.network.models.Address
+import com.example.petrescueapp.data.network.models.ApiAnimals
+import com.example.petrescueapp.data.network.models.Contact
+import com.example.petrescueapp.domain.models.Pet
+import com.example.petrescueapp.domain.models.PetOwnerContacts
+
+class PetApiMapperImplementation:PetApiMapper<List<Pet>,ApiAnimals> {
+    companion object{
+        private const val EMPTY_DATA = "unknown"
+    }
+
+    override fun mapToDomain(apiEntity: ApiAnimals): List<Pet> {
+        return apiEntity.animals.map { animal ->
+            animal.run{
+                Pet(
+
+                )
+           }
+        }
+    }
+
+
+    private fun formatContacts(contact:Contact?):PetOwnerContacts{
+        return PetOwnerContacts(
+            address=formatData(formatAddress(contact?.address))
+        )
+    }
+
+    private fun formatAddress(address: Address?):String{
+        //a period/full stop to help separate the address components
+        val dot = "u25CF"
+        if(address != null){
+            return "${address.city}$dot${address.country}"
+        }
+        return ""
+    }
+
+    private fun formatData(data:String?):String{
+        return data ?: EMPTY_DATA
+    }
 }
+
