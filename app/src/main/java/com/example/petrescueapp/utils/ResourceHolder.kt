0d375f5e977ccb817cf15316de730bb38357165f
MBANS8A1 <seanmbng@aol.com>
2025-07-08 20:55:18 +0100

    In the ResourceHolder.kt, I have made the ResourceHolder class a sealed class as I know the subclasses will only deal with specific states when retrieving data from the data source. The subclasses are Loading, Success and Error, representing states with the same name.  I can receive any of these states when I make a network request. I made them generics of type T so they can work with any kind of data, not just animal/pet data.

--- app/src/main/java/com/example/petrescueapp/utils/ResourceHolder.kt
+++ app/src/main/java/com/example/petrescueapp/utils/ResourceHolder.kt
@@ -1,4 +1,20 @@
 package com.example.petrescueapp.utils
 
-sealed class ResourceHolder {
+sealed class ResourceHolder<T>(
+    val data : T? = null,
+    val throwable: Throwable? = null
+){
+
+    class Loading<T>:ResourceHolder<T>(){
+
+    }
+    //never going to be nullable if I get data back
+    class Success<T>(data: T):ResourceHolder<T>(data=data){
+
+    }
+
+    class Error<T>(throwable: Throwable?):ResourceHolder<T>(throwable = throwable){
+
+    }
+
 }
