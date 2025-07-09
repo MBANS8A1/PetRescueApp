49dcd2e417f6af07a2e98d4c7778b85b3de41ae5
MBANS8A1 <seanmbng@aol.com>
2025-07-09 11:18:07 +0100

    In the Graph.kt file, I have added variables for the AccessTokenProvider, PetRepository and PetApiMapperImplementation. Also, through lazy initialization, I have made an okhttp3 client on line 33 using a Builder along with the custom authenticator and interceptor; this is to ensure only a single instance of the client is used to avoid waste of connection resources and to ensure it is initialized to nothing when it unused. Next, I used the client with Retrofit on line 50 of the createPetFinderApiService() method, so it can use this client to obtain the needed access token to retrieve the animal data.

--- app/src/main/java/com/example/petrescueapp/Graph.kt
+++ app/src/main/java/com/example/petrescueapp/Graph.kt
@@ -3,10 +3,18 @@ package com.example.petrescueapp
 import android.content.Context
 import com.example.petrescueapp.data.local.StoragePref
 import com.example.petrescueapp.data.network.baseObject
+import com.example.petrescueapp.data.network.interceptors.AccessInterceptor
+import com.example.petrescueapp.data.network.interceptors.AccessTokenAuthorization
+import com.example.petrescueapp.data.network.mappers.PetApiMapper
+import com.example.petrescueapp.data.network.mappers.PetApiMapperImplementation
 import com.example.petrescueapp.data.network.retrofit.PetFinderApiService
+import com.example.petrescueapp.data.network.token.AccessTokenProvider
+import com.example.petrescueapp.data.repository.PetRepositoryImplementation
+import com.example.petrescueapp.domain.repository.PetRepository
 import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
 import kotlinx.serialization.json.Json
 import okhttp3.MediaType.Companion.toMediaType
+import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 
 private val json = Json { ignoreUnknownKeys = true }
@@ -17,6 +25,18 @@ object Graph {
     lateinit var tokenStoragePref : StoragePref
     //For getting an authorised token and list of animals
     lateinit var apiService :  PetFinderApiService
+    //For saving,refreshing,getting the token String and fetching the access token
+    lateinit var accessTokenProvider: AccessTokenProvider
+    //Get the animal(s) via entity-domain mapping
+    lateinit var petRepository: PetRepository
+    val apiMapper = PetApiMapperImplementation()
+    private val okHttpClient:OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .addInterceptor(AccessInterceptor(accessTokenProvider))
+            .authenticator(AccessTokenAuthorization(accessTokenProvider))
+            .build()
+    }
+
 
     fun provide(context:Context){
         tokenStoragePref = StoragePref(context)
@@ -27,6 +47,7 @@ object Graph {
         //telling retrofit data will come to me in the form of JSON
         val contentType = "application/json".toMediaType()
         return Retrofit.Builder()
+            .client(okHttpClient)
             .baseUrl(baseObject.BASE_URL)
             .addConverterFactory(json
                     .asConverterFactory(contentType)
