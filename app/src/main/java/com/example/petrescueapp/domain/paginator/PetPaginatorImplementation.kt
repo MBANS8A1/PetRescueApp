45e6cc4d647589bd283ea2c51de637aeaba5c214
MBANS8A1 <seanmbng@aol.com>
2025-07-09 15:22:50 +0100

    To the PetPaginatorImplementation generic class, I have added as dependencies (parameters), initialKey(just the starting page) and the loadingState (however, I will probably need to create an interface separately to manage the state).

--- app/src/main/java/com/example/petrescueapp/domain/paginator/PetPaginatorImplementation.kt
+++ app/src/main/java/com/example/petrescueapp/domain/paginator/PetPaginatorImplementation.kt
@@ -1,4 +1,16 @@
 package com.example.petrescueapp.domain.paginator
 
-class PetPaginatorImplementation {
+class PetPaginatorImplementation<Page,Result>(
+    private val initialKey: Page,
+    private val loadingState
+):PetPaginator<Page,Result>
+
+ {
+    override suspend fun fetchNextPage() {
+        TODO("Not yet implemented")
+    }
+
+    override fun resetPage() {
+        TODO("Not yet implemented")
+    }
 }
