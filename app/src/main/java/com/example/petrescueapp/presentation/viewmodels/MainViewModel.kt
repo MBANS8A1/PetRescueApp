1ff4818d90651de420de0b3dd7ae26113c1f32f0
MBANS8A1 <seanmbng@aol.com>
2025-07-10 12:26:29 +0100

    In my MainViewModel.kt file, I created another data class called Uistate to keep track of the different state conditions (properties/variables that will be important when the lifecycle of the UI Controller begins). There is a variable for the list of Pets, which is initialized to ResourceHolder.Loading to reflect loading of the Pet(animal) data along with boolean conditions for whether more Pet information needs to be fetched, the visibility of a button to load more Pet information and to control scrolling.

--- app/src/main/java/com/example/petrescueapp/presentation/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/petrescueapp/presentation/viewmodels/MainViewModel.kt
@@ -1,10 +1,24 @@
 package com.example.petrescueapp.presentation.viewmodels
 
+import androidx.compose.runtime.mutableStateOf
 import androidx.lifecycle.ViewModel
 import com.example.petrescueapp.Graph
+import com.example.petrescueapp.data.network.models.ApiAnimals
+import com.example.petrescueapp.domain.models.Pet
 import com.example.petrescueapp.domain.repository.PetRepository
+import com.example.petrescueapp.utils.ResourceHolder
 
 class MainViewModel(
     private val repository: PetRepository =  Graph.petRepository
 ):ViewModel() {
+
+    var uiState by mutableStateOf(Uistate)
 }
+
+
+data class Uistate(
+    val animals: ResourceHolder<List<Pet>> = ResourceHolder.Loading(),
+    val isFetchingPet: Boolean = false,
+    val loadMoreButton: Boolean = true,
+    val startInfiniteScrolling: Boolean = false
+)
