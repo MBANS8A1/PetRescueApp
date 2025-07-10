4b91053e2f556d5b2098a797e45b5d3173d622d9
MBANS8A1 <seanmbng@aol.com>
2025-07-10 18:19:40 +0100

    For the petPaginator variable in the body of the MainViewModel, I need to provide the properties the PetPaginatorImplementation needs. The first is the initialKey; I made a getPage() helper function, which uses a pageSource as a parameter; if there is no Pet data in the pagesSource list, the currentPage of the last index is incremented by 1. However, if it is not empty 1 is returned as the page.

--- app/src/main/java/com/example/petrescueapp/presentation/viewmodels/MainViewModel.kt
+++ app/src/main/java/com/example/petrescueapp/presentation/viewmodels/MainViewModel.kt
@@ -6,6 +6,8 @@ import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
 import com.example.petrescueapp.Graph
 import com.example.petrescueapp.domain.models.Pet
+import com.example.petrescueapp.domain.paginator.PetPaginator
+import com.example.petrescueapp.domain.paginator.PetPaginatorImplementation
 import com.example.petrescueapp.domain.repository.PetRepository
 import com.example.petrescueapp.utils.ResourceHolder
 
@@ -17,10 +19,18 @@ class MainViewModel(
     companion object{
         const val TAG = "myModel"
     }
+    private val petPaginator: PetPaginatorImplementation(
+        initialKey = getPage(uiState.animals.data),
+
+    )
+
+    private fun getPage(pageSource: List<Pet>?):Int{
+       return if(pageSource?.isEmpty() == true){
+            pageSource[pageSource.lastIndex].currentPage + 1
+        }else 1
+    }
 
 }
-
-
 data class Uistate(
     val animals: ResourceHolder<List<Pet>> = ResourceHolder.Loading(),
     val isFetchingPet: Boolean = false,
