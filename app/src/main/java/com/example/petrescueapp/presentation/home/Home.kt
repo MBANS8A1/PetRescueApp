aff3041ed8723b010052b0b5f594962df4246295
MBANS8A1 <seanmbng@aol.com>
2025-07-12 22:15:22 +0100

    In the Home composable, within the itemsIndexed block I have added a LaunchedEffect API with the key1 argument being the scrollState, as that is the state I want to monitor before triggering the onLoadNextPage() coroutine when composition occurs. If the last Pet item index has been reached, the user has scrolled to the bottom of the screen and another data fetching request is not being triggered (important to prevent unnecessary consumption of network resources), then the next page of the application will be loaded.

--- app/src/main/java/com/example/petrescueapp/presentation/home/Home.kt
+++ app/src/main/java/com/example/petrescueapp/presentation/home/Home.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.lazy.rememberLazyListState
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -58,6 +59,15 @@ fun Home(
                                 onPetClick.invoke(index)
                             }
                         )
+                        LaunchedEffect(key1 = scrollState) {
+                            if(index >= petList.lastIndex &&
+                                !uistate.isFetchingPet &&
+                                uistate.startInfiniteScrolling
+
+                                ){
+                                    onLoadNextPage()
+                            }
+                        }
                     }
                 }
             }
