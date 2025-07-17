f08b9b9f0b3a997e60e9cf734e6cf715e3aa7cb6
MBANS8A1 <seanmbng@aol.com>
2025-07-17 09:42:28 +0100

    In the Navigation.kt file, I added to the PetRescueNavigation composable function the NavHostController and the uistate. The NavHost container for the NavGraph navigation and screen displays has been given the navController argument and been passed the startDestination String of Screen.Home.name; this is after the Screen enum was moved from the MainActivity.kt file to the Navigation.kt file. Next, the composable(){} was nested inside the NavHost to link the Home() composable (I cut and pasted from the MainActivity.kt file) to the route Screen.Home.name. As I will not be navigating based off of state variables, I removed the currentScreen mutable state reference within the onPetClick lambda function.

--- app/src/main/java/com/example/petrescueapp/MainActivity.kt
+++ app/src/main/java/com/example/petrescueapp/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.compose.rememberNavController
 import com.example.petrescueapp.presentation.detail.DetailScreen
 import com.example.petrescueapp.presentation.home.Home
 import com.example.petrescueapp.presentation.ui.theme.PetRescueAppTheme
@@ -35,6 +36,7 @@ class MainActivity : ComponentActivity() {
             var id by remember{
                 mutableIntStateOf(-1)
             }
+            val navController  = rememberNavController()
             PetRescueAppTheme(
                 darkTheme = isDarkTheme
             ) {
@@ -43,22 +45,7 @@ class MainActivity : ComponentActivity() {
                     ){
                     when(currentScreen){
                         Screen.Home ->{
-                            Home(
-                                uistate = viewModel.uiState,
-                                onSwitchClick = { isDarkTheme = !isDarkTheme
-                            },
 
-                                onPetClick ={ selectedId->
-                                    currentScreen = Screen.Detail
-                                    id = selectedId
-                                } ,
-                                onLoadNextPage = viewModel::loadNextPetsPage,
-                                onInfiniteScrollingChange = {
-                                    viewModel.onInfiniteScrollChange(it)
-                                }
-
-
-                            )
                         }
                         Screen.Detail->{
                             DetailScreen(
@@ -75,7 +62,3 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-enum class Screen{
-    Home,
-    Detail
-}
--- app/src/main/java/com/example/petrescueapp/presentation/navigation/Navigation.kt
+++ app/src/main/java/com/example/petrescueapp/presentation/navigation/Navigation.kt
@@ -1,9 +1,43 @@
 package com.example.petrescueapp.presentation.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.petrescueapp.presentation.home.Home
 
+enum class Screen{
+    Home,
+    Detail
+}
 
 @Composable
-fun PetRescueNavigation() {
+fun PetRescueNavigation(
+    navController: NavHostController,
+    uistate: UiState
+) {
+    NavHost(navController = navController,
+           startDestination = Screen.Home.name,
 
+        ){
+            composable(route=Screen.Home.name){
+                Home(
+                    uistate = uistate,
+                    onSwitchClick = { isDarkTheme = !isDarkTheme
+                    },
+
+                    onPetClick ={ selectedId->
+
+                    } ,
+                    onLoadNextPage = viewModel::loadNextPetsPage,
+                    onInfiniteScrollingChange = {
+                        viewModel.onInfiniteScrollChange(it)
+                    }
+
+
+                )
+            }
+    }
 }
+
